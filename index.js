43294bbd8febeb2c8c6e356cf4c974099d95f273
Ikmal Syifai <ikmal.s.16@gmail.com>
2017-07-18 11:08:53 +0700

    Add pub/sub-queue

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ module.exports = function () {
             container.register("serviceApi", require("./lib/service_api"));
             container.register("servicePub", require("./lib/service_pub"));
             container.register("serviceSub", require("./lib/service_sub"));
+            container.register("servicePubQueue", require("./lib/service_pub_queue"));
+            container.register("serviceSubQueue", require("./lib/service_sub_queue"));
         },
 
         *onInit(container) {
             let service = yield container.resolve("service");
             let servicePub = yield container.resolve("servicePub");
             let serviceSub = yield container.resolve("serviceSub");
+            let servicePubQueue = yield container.resolve("servicePubQueue");
+            let serviceSubQueue = yield container.resolve("serviceSubQueue");
             let serviceApi = yield container.resolve("serviceApi");
 
             service.addModule("api", serviceApi);
             service.addModule("pub", servicePub);
             service.addModule("sub", serviceSub);
+            service.addModule("pub-queue", servicePubQueue);
+            service.addModule("sub-queue", serviceSubQueue);
         },
 
         *onStart(container) {
